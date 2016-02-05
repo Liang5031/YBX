f1015827100b0dbb19c72cf1020b3aad4bbd251d
chenliang <chenliang5031@gmail.com>
2016-02-06 00:12:42 +0800

    add reset password activity

--- Guider/app/src/main/java/com/ybx/guider/activity/LoginActivity.java
+++ Guider/app/src/main/java/com/ybx/guider/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.ybx.guider.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -13,7 +15,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.ybx.guider.R;
 import com.ybx.guider.VolleyRequestQueue;
 
-public class LoginActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener{
+public class LoginActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener {
     private TextView tv;
 
     @Override
@@ -21,24 +23,26 @@ public class LoginActivity extends AppCompatActivity implements Response.Listene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-//        tv = (TextView)this.findViewById(R.id.response);
-        String url ="http://www.baidu.com";
 
-        // Request a string response from the provided URL.
-//        XMLRequest xmlRequest = new XMLRequest(Request.Method.GET, url, this, this);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,this,this);
-        // Add the request to the RequestQueue.
-        VolleyRequestQueue.getInstance(this).add(stringRequest);
+//        String url ="http://www.baidu.com";
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,this,this);
+//        VolleyRequestQueue.getInstance(this).add(stringRequest);
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-        tv.setText("That didn't work!");
+//        tv.setText("That didn't work!");
     }
 
     @Override
     public void onResponse(String response) {
         // Display the first 500 characters of the response string.
 //        tv.setText("Response is: "+ response.substring(0,500));
+    }
+
+    public void onClickForgot(View view) {
+        Intent intent = new Intent(this, ResetPasswordActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
---
+++ Guider/app/src/main/java/com/ybx/guider/activity/ResetPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.ybx.guider.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ybx.guider.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_passward);
+    }
+}
